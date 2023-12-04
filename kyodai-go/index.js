f8a3a9fc03ca6138d7e571f4e3cb9380bc23ddde
Aoi <aoi.hosomi.218@gmail.com>
2023-12-04 19:46:36 +0900

    ああを追加

--- kyodai-go/index.js
+++ kyodai-go/index.js
@@ -15,7 +15,7 @@ let profileButton, escapeButton, focusImg, encounterLog, quizDiv;
 let question, opt1, opt2, opt3, opt4, quizResult;
 
 let editButton;
-let profileImg, profileNameText, profileNameInput;
+let profileImg, profileNameText, profileNameInput;ああ
 let creditNum = 0;
 let myDegree;
 
