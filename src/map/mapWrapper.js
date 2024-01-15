e6c9447b7409743a92ee55f1c50fb99b94d77cb4
Aoi <aoi.hosomi.218@gmail.com>
2024-01-15 20:07:07 +0900

    timinigクラスとmapWrapperクラスを作成

---
+++ src/map/mapWrapper.js
@@ -0,0 +1,61 @@
+class MapWrapper {
+    #gmap;
+
+    constructor(gmap) {
+        this.#gmap = gmap;
+    }
+
+    display(mark, onClick) {
+        const content = document.createElement("img");
+        content.className = "obj-mark";
+        content.src = mark.imageUrl;
+        const markerElement = new google.maps.marker.AdvancedMarkerElement({
+            map: gmap,
+            position: mark.position.getGoogleMapLatLng(),
+            content: content,
+        });
+
+        markerElement.addListener("click", function() {
+            onClick(markerElement);
+        });
+
+        return markerElement;
+    }
+
+    // placedMarks[i] = {
+    //     pos,
+    //     obj,
+    //     marker,
+    // };
+
+    // setTimeout(function() {
+    //     content.classList.add("stay");
+    // }, 500);
+
+    // console.log("出現", placedMarks);
+    
+    // let placedMark = placedMarks[i];
+
+    // const { stayMin } = adjustment().appearance;
+    // const stayTime = _randomNum(stayMin[0], stayMin[1])*60*1000;
+
+    // setTimeout(() => {
+    //     _removeMark(placedMark, i);
+    // }, stayTime);
+
+    
+    hide(markerElement) {
+        markerElement.setMap(null);
+    }
+
+    // let content = document.createElement("img");
+    // content.classList.remove("stay");
+    // setTimeout(function() {
+    //     if(placedMarks[i] === placedMark) {
+    //         placedMarks[i].mark.setMap(null);
+    //         placedMarks[i] = null;
+    //         console.log("消滅", placedMarks)
+    //         placeMark(i);
+    //     }
+    // }, 700);
+}
--- src/map/timing.js
+++ src/map/timing.js
@@ -1,23 +1,34 @@
 class Timing {
     #intervalSec;
     #stayMin;
-
-    constructor(intervalSec, stayMin){
+    constructor(intervalSec, stayMin) {
         this.#intervalSec = intervalSec;
         this.#stayMin = stayMin;
     }
 
     #randomNum(min, max) {
-        return Math.random()*(max - min) + min;
+        return Math.random() * (max - min) + min;
     }
     
-    #setInterval() {
-        const [min, max] = this.#intervalSec;
-        return this.#randomNum(min, max);
+    #getIntervalTime() {
+        const [minSec, maxSec] = this.#intervalSec;
+        return this.#randomNum(minSec, maxSec) * 1000;
+        
     }
+    #getStayTime() {
+        const [minMin, maxMin] = this.#stayMin;
+        return this.#randomNum(minMin, maxMin) * 60 * 1000;
+        
+    }
+    run(markDealer, map, onClick) {
+        const mark = markDealer.deal();
+        setTimeout(() => {
+            const markerElement = map.display(mark, onClick);
 
-    #setStay() {
-        const [min, max] = this.#stayMin;
-        return this.#randomNum(min, max);
+            setTimeout(() => {
+                map.hide(markerElement);
+                this.run(markDealer, map, onClick);
+            }, this.#getStayTime());
+        }, this.#getIntervalTime());
     }
 }
